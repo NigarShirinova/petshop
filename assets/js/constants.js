6e106f5c6d26ac46cf44e254b2a3ff5dbfdd9ce4
NigarShirinova <nigaresh@code.edu.az>
2024-10-05 12:37:39 +0400

    updated

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "json-seven-jade.vercel.app";
+export const API_BASE_URL = "https://json-server-ten-lake.vercel.app/";
 export const endpoints={
     catalog: "/catalog",
 };
